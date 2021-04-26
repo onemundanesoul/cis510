715138e5de0d6a72882fc750900b0d14387f4640
onemundanesoul <65574360+onemundanesoul@users.noreply.github.com>
2021-04-26 10:57:25 -0700

    Update cis510.py

--- cis510.py
+++ cis510.py
@@ -1,1 +1,8 @@
 
+def remove_list_item(*, the_list, the_item):
+  #the_list because list is a Python function
+  '''
+  Use three apostrophes to mark off larger comments.
+  '''
+  new_list = [item for item in the_list if item != the_item]
+  return new_list
